9726b493640acb93b152284e83647e3a85e34f75
mohamed gassama <bouyagassama@gmail.com>
2020-01-14 18:45:53 -0500

    db Url conneced

--- server/src/serverConfig/dev.ts
+++ server/src/serverConfig/dev.ts
@@ -1,6 +1,6 @@
 export const config = {
   mongoDbUrl:
-    'mongodb://alumni_network_bouyagas:alumni_network_bouyagas1@ds239206.mlab.com:39206/alumni_network',
+    'mongodb+srv://kakygas:kakygas1989>@alumninetwork-9wxs0.mongodb.net/test?retryWrites=true&w=majority',
   secrets: {
     jwt: 'devSecret',
   },
